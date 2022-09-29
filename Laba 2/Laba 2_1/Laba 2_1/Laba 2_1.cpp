c469ae2d341a7f16af4f89529b70fcd325054dba
Stas <112703038+silenthnowakeup@users.noreply.github.com>
2022-09-29 19:02:14 +0300

    Add files via upload

---
+++ Laba 2/Laba 2_1/Laba 2_1/Laba 2_1.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	long int n = 1, fac = 1, k = 1, z = 0;
+
+	printf("Enter factorial=");
+	// Вводим число
+	scanf("%d/n", &n);
+	//Цикл который находит факториал числа
+	for (int i = 1; i < n + 1; i++) {
+		fac *= i;
+	}
+	//Цикл который находит факториал в виде произведения трех последних членов
+	for (k = 1; k * k * k - k <= fac; k++) {
+		if (k * k * k - k == fac) { z = 1; break; }
+	}
+	if (z == 1) { printf("%d %d %d", k - 1, k, k + 1); }
+	else { printf("No find"); }
+
+	return 0;
+}
---
+++ Laba 2/Laba 2_3/Laba 2_3/Laba 2_3.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main() {
+	double S = 0, z, x, b;
+	int k, n;
+	long long int fac = 1;
+	//Ввод угла
+	printf("Enter x [-90;90]= ");
+	scanf_s("%lf", &x);
+	// Перевод угла из радиан в градусы
+	z = x / 180 * acos(-1);
+	//Ввод до какого числа будет суммироваться 
+	printf("Enter n= ");
+	scanf_s("%d", &n);
+	//Цикл который находит синус угла через разложение в ряд Тейлора
+	for (k = 0; k <= n; k++) {
+		//Цикл который находит факториал числа
+		for (int i = 1; i <= 2 * k + 1; i++) {
+			fac *= i;
+		}
+
+		b = pow(-1, k) * pow(z, 2 * k + 1) / fac;
+		S = S + b;
+		fac = 1;
+	}
+	printf("Sin(X)= %lf", S);
+	return 0;
+}
