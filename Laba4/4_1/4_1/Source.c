82537ec66b196e186f5f068269309659bdce4ec6
Stas <112703038+silenthnowakeup@users.noreply.github.com>
2022-12-02 17:21:24 +0300

    Add files via upload

---
+++ Laba4/4_1/4_1/Source.c
@@ -0,0 +1,160 @@
+﻿#include <stdio.h>
+// ввод массива
+void input(int arr[100][100], int x) {
+	for (int i = 0; i < x; i++) {
+		for (int j = 0; j < x; j++) {
+			printf("arr[%d][%d] = ", i, j);
+			while (scanf_s("%d", &arr[i][j]) != 1 || getchar() != '\n')
+			{
+				printf("Wrong input!\n");
+				rewind(stdin);
+			}
+
+		}
+	}
+}
+//функция ввода случайных элементов
+void randinput(int arr[100][100], int x) {
+	srand(time(NULL));
+	for (int i = 0; i < x; i++) {
+		for (int j = 0; j < x; j++) {
+			arr[i][j] = rand() % 10;   //Генерация рандомных чисел от 9
+			printf("arr[%d][%d] =%d\n", i, j, arr[i][j]);
+		}
+	}
+}
+//функция суммы элеметов массива
+void summa(int arr[100][100], int x) {
+	int sum = 0;
+	for (int i = 0; i < x; i++) {
+		for (int j = 0; j < x; j++) {
+
+			if (&arr[j][i] > 0) {
+				sum += arr[j][i];
+			}
+		}
+	}
+	printf("sum = %d\n", sum);
+}
+//вывод массива
+void output(int arr[100][100], int x) {
+	for (int i = 0; i < x; i++) {
+		for (int j = 0; j < x; j++) {
+			printf(" %d ", arr[i][j]);
+
+		}
+		printf("\n");
+	}
+}
+//Функция, которая выводит элементы побочной диагонали
+void diagonal(int arr[100][100], int x)
+{
+	printf("Diagonal:");
+	for (int f = 0; f < x; f++)
+	{
+		printf("%d ", arr[x - 1 - f][f]);
+	}
+}
+//ЭЛЕМЕНТЫ ВЕРХНЕЙ ДИАГОНАЛИ
+int topdiagonal(int arr[100][100], int x) {
+	int s[100] = { 0 }, sum2;
+	//Цикл, который ищет диагонали, параллельные побочной
+	for (int r = 2; r <= x; r++)
+	{
+		for (int f = 0; f <= x - r; f++) {
+			if (arr[x - r - f][f] < 0) {
+				arr[x - r - f][f] *= -1;
+			}
+			s[r] += arr[x - r - f][f];
+		}
+	}
+	sum2 = s[2];
+	for (int r = 2; r <= x; r++) {
+		if (s[r] < sum2) {
+			sum2 = s[r];
+		}
+	}
+	return sum2;
+}
+//ЭЛЕМЕНТЫ НИЖНЕЙ ДИАГОНАЛИ
+int underdiagonal(int arr[100][100], int x) {
+	int s2[100] = { 0 }, sum3;
+	for (int r = x - 2; r >= 0; r--)
+	{
+		for (int f = x - 1; f > r; f--) {
+			if (arr[f][x + r - f] < 0) {
+				arr[f][x + r - f] *= -1;
+			}
+			s2[r] += arr[f][x + r - f];
+		}
+	}
+	sum3 = s2[0];
+	for (int r = 0; r <= x - 2; r++) {
+		if (s2[r] < sum3) {
+			sum3 = s2[r];
+		}
+	}
+	return sum3;
+}
+int main() {
+	int arr[100][100] = { 0 }, i, j, sum = 0, k = 0, sum2 = 0, sum3 = 0, s[100] = { 0 }, s2[100] = { 0 }, x;
+	float operation;
+	printf("Lenght of array = ");
+	while (scanf_s("%d", &x) != 1 || getchar() != '\n' || x > 100 || x < 0)
+	{
+		printf("Wrong input!\n");
+		rewind(stdin);
+	}
+
+	printf("\nChoose operation:\n");
+	printf("1) Input element\n");
+	printf("2) Random element\n");
+	while (scanf_s("%f", &operation) != 1 || getchar() != '\n' || operation > 2 || operation < 0 || operation != (int)operation)
+	{
+		printf("Wrong input!\n");
+		rewind(stdin);
+	}
+
+	switch ((int)operation)
+	{
+	case 1: {
+		// ввод массива
+		input(arr, x);
+		//функция суммы элеметов массива
+		summa(arr, x);
+		//вывод массива
+		output(arr, x);
+		//Функция, которая выводит элементы побочной диагонали
+		diagonal(arr, x);
+		//ЭЛЕМЕНТЫ ВЕРХНЕЙ ДИАГОНАЛИ
+		sum2 = topdiagonal(arr, x);
+		//ЭЛЕМЕНТЫ НИЖНЕЙ ДИАГОНАЛИ
+		sum3 = underdiagonal(arr, x);
+		if (sum2 < sum3) {
+			printf("\nMinimum diagonal = %d", sum2);
+		}
+		else printf("\nMinimum diagonal = % d", sum3);
+		break;
+	}
+	case 2: {
+		//функция ввода случайных элементов
+		randinput(arr, x);
+		//функция суммы элеметов массива
+		summa(arr, x);
+		//вывод массива
+		output(arr, x);
+		//Функция, которая выводит элементы побочной диагонали
+		diagonal(arr, x);
+		//ЭЛЕМЕНТЫ ВЕРХНЕЙ ДИАГОНАЛИ
+		sum2 = topdiagonal(arr, x);
+		//ЭЛЕМЕНТЫ НИЖНЕЙ ДИАГОНАЛИ
+		sum3 = underdiagonal(arr, x);
+		if (sum2 < sum3) {
+			printf("\nMinimum element in diagonal = %d", sum2);
+		}
+		else printf("\nMinimum element in diagonal = % d", sum3);
+		break;
+	}
+	}
+	return 0;
+}
---
+++ Laba4/4_2/4_2/Source.c
@@ -0,0 +1,105 @@
+﻿#include <stdio.h>
+void input(int arr[100][100], int col, int row) {
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			printf("arr[%d][%d] = ", i, j);
+			while (scanf_s("%d", &arr[i][j]) != 1 || getchar() != '\n')
+			{
+				printf("Wrong input!\n");
+				rewind(stdin);
+			}
+
+		}
+	}
+}
+//функция ввода случайных элементов
+void randinput(int arr[100][100], int col, int row) {
+	srand(time(NULL));
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			arr[i][j] = rand() % 10;   //Генерация рандомных чисел от 9
+			printf("arr[%d][%d] =%d\n", i, j, arr[i][j]);
+		}
+	}
+}
+//вывод массива
+void output(int arr[100][100], int col, int row) {
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			printf(" %d ", arr[i][j]);
+
+		}
+		printf("\n");
+	}
+}
+int searchnull(int arr[100][100], int col, int row) {
+	int k = 0, remj;
+	//цикл, который ищет, равны ли в столбце все элементы нулю 
+	for (int j = 0; j < col; j++) {
+		for (int i = 0; i < row; i++)
+		{
+			if (arr[i][j] == 0) {
+				k++;
+			}
+		}
+		if (k == row)
+		{
+			remj = j;
+		}
+		else
+		{
+			printf("THERE IS NO COLUMN IN WHICH ALL ELEMENTS ARE NULL\n");
+			return 0;
+		}
+		//цикл, который умножает на (-1) строку с таким же номером, как и столбец (если удовлетворяет условию)
+		for (j = 0; j < col; j++) {
+			arr[remj][j] *= -1;
+		}
+		break;
+	}
+	printf("j = %d\n", remj);
+	return remj;
+}
+void outputres(int arr[100][100], int col, int row) {
+	printf("Result:\n");
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+}
+int main() {
+	int arr[100][100] = { 0 }, i, col, row, j, k = 0, remj, operation;
+	printf("column = ");
+	scanf_s("%d", &col);
+	printf("row = ");
+	scanf_s("%d", &row);
+	printf("\nChoose operation:\n");
+	printf("1) Input element\n");
+	printf("2) Random element\n");
+	scanf_s("%d", &operation);
+	switch (operation)
+	{
+	case 1: {
+		// ввод массива
+		input(arr, col, row);
+		//вывод массива
+		output(arr, col, row);
+		searchnull(arr, col, row);
+		outputres(arr, col, row);
+		break;
+	}
+	case 2:
+	{
+		randinput(arr, col, row);
+		//вывод массива
+		output(arr, col, row);
+		searchnull(arr, col, row);
+		//вывод результата 
+		outputres(arr, col, row);
+	}
+	break;
+	}
+	return 0;
+}
