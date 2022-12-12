54dce00a22add1c51f81fcebe86f99da6d957041
Stas <112703038+silenthnowakeup@users.noreply.github.com>
2022-12-12 22:11:11 +0300

    Update main.c

--- untitled5/main.c
+++ untitled5/main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char* getStr1(char* s1)
+char* getStr(char* s1)
 {
     char temp;
     int i = 0;
+    int prSize = 16;
+    int size = 0;
+    s1 = (char*)calloc(prSize,sizeof(char));
     while ((temp = getchar()) != '\n' && temp != EOF)
     {
         s1[i] = temp;
         i++;
+        if (i > 15)
+        {
+            size += 2;
+            s1 = (char*)realloc(s1, size * sizeof(char));
+        }
     }
     s1[i] = '\0';
 
@@ -35,20 +43,6 @@ int str_len(char* s1)
     return i;
 }
 
-char* getStr2(char* s2)
-{
-    char temp;
-    int i = 0;
-    while ((temp = getchar()) != '\n' && temp != EOF)
-    {
-        s2[i] = temp;
-        i++;
-    }
-    s2[i] = '\0';
-
-    return s2;
-}
-
 char* str_cat(char* s1, char* s2, int k, int m)
 {
     char* s;
@@ -73,7 +67,7 @@ char* str_cat(char* s1, char* s2, int k, int m)
 int myf(char* str)
 {
     int size = str_len(str);
-    int sum = 0, n = 0, znak = 1;
+    int sum = 0, n = 0, znak = 1, res = 0;
     for (int i = 0; i < size + 1; i++)
     {
         if (i == 0 || str[i - 1] >= 'a' && str[i] <= 'z' || str[i - 1] >= 'A' && str[i] <= 'Z' || str[i - 1] == ' ')
@@ -111,16 +105,17 @@ int Correct_input_task(void) {
 }
 
 int contCheck() {
-    char temp = 0;
     int CHECK = 0;
-    temp = getchar();
     printf("\n<Yes(Y)>\t");
     printf("<No(N)>\n");
     printf(">");
+    char temp = 0;
+    temp = getchar();
     while ((temp = getchar()) != '\n') {
         CHECK = 0;
         while (temp != 'N' && temp != 'n' && temp != 'Y' && temp != 'y')
         {
+
             rewind(stdin);
             printf("Incorrect input\n");
             printf(">");
@@ -129,6 +124,7 @@ int contCheck() {
         if (temp == 'n' || temp == 'N')
         {
             CHECK = 1;
+
         }
     }
     return CHECK;
@@ -136,36 +132,27 @@ int contCheck() {
 
 void Task1()
 {
-    do
-    {
         char* str;
-        str = (char*)calloc(1,sizeof(char));
         printf("Input your string::");
-        str = getStr1(str);
+        str = getStr(str);
         printf("\nsumma = %d \n", myf(str));
-    } while (contCheck() != 1);
 }
 
 void Task2()
 {
-    do
-    {
         char* s1;
         char* s2;
         int k, m;
         printf("Input first string::");
-        s1 = (char*)calloc(1,sizeof(char));
-        s1 = getStr1(s1);
-        s2 = (char*)calloc(1,sizeof(char));
+        s1 = getStr(s1);
         printf("\nInput second string::");
-        s2 = getStr1(s2);
+        s2 = getStr(s2);
         printf("Enter k::");
         scanf("%d", &k);
         printf("\nEnter m::");
         scanf("%d", &m);
         s1 = str_cat(s1, s2, k, m);
         outStr(s1);
-    } while (contCheck() != 1);
 }
 
 void endTask()
