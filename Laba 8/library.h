64787dae1fa68a3204c46828894969f12b091d83
Stas <112703038+silenthnowakeup@users.noreply.github.com>
2022-12-12 11:19:56 +0300

    Add files via upload

---
+++ Laba 8/library.h
@@ -0,0 +1,14 @@
+//
+// Created by stasc on 09.12.2022.
+//
+
+#ifndef LABA_8_LIBRARY_H
+#define LABA_8_LIBRARY_H
+int length(char*);
+void merge_sort(int*, int, int);
+char** sorting(char**, int*, int);
+int getSize(char*);
+char* copyString(char*, char*);
+void printArr(int*, int);
+void printStrings(int, char**);
+#endif //LABA_8_LIBRARY_H
